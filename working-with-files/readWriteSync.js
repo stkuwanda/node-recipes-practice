093591424870b5ddd242aab12d22a68bfdc98d06
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-19 17:42:14 +0200

    added special comment on the fs.readFileSync 2nd optional argument

--- working-with-files/readWriteSync.js
+++ working-with-files/readWriteSync.js
@@ -4,7 +4,7 @@ const path = require('node:path');
 // const filePath = path.join(process.cwd(), 'hello.txt');
 const filePath = path.resolve(process.cwd(), 'hello.txt'); // | path.resolve(process.cwd(),'./', 'hello.txt');
 
-const contents = fs.readFileSync(filePath, 'utf8');
+const contents = fs.readFileSync(filePath, 'utf8');// without the utf8 argument the call returns a Buffer
 console.log('File contents:', contents);
 
 const contentToUppercase = contents.toUpperCase();
