0e113aeff06fcaa8dc01fbbd6dd23179da82506d
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-19 17:23:21 +0200

    overwrote text file with updated content

--- working-with-files/readWriteSync.js
+++ working-with-files/readWriteSync.js
@@ -4,4 +4,8 @@ const path = require('node:path');
 const filePath = path.join(process.cwd(), 'hello.txt');
 
 const contents = fs.readFileSync(filePath, 'utf8');
-console.log('File contents:\n', contents);
+console.log('File contents:', contents);
+
+const contentToUppercase = contents.toUpperCase();
+fs.writeFileSync(filePath, contentToUppercase); // overwrites the file with updated text
+console.log('File update.');
