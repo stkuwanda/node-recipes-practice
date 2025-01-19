cbae562d33dc6fcc84f7f13a3aada5295dc7cff3
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-19 10:58:01 +0200

    added simple error handling by outputting a message to the stdout error output stream

--- interfacing-with-io/greeting.js
+++ interfacing-with-io/greeting.js
@@ -4,5 +4,10 @@ process.stdin.on('data', function (data) {
   // processing on each data event
   // console.log(data); // data is a Buffer object
   const nameVar = data.toString().trim().toUpperCase();
-  process.stdout.write(`Hi ${nameVar}!`);
+
+  if(nameVar !== ''){
+    process.stdout.write(`Hi ${nameVar}!\n`); // writes to the output stream
+  } else {
+    process.stderr.write('Input is empty!\n'); // writes to the error output stream
+  }
 });
