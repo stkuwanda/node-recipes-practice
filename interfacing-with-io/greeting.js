ce690db53d9f3479fe54adaaeda92e19e4ba8461
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-19 10:51:39 +0200

    read data and wrote a string message to the stdout output stream

--- interfacing-with-io/greeting.js
+++ interfacing-with-io/greeting.js
@@ -1,5 +1,8 @@
 console.log('What\'s your name?');
 
 process.stdin.on('data', function (data) {
-
+  // processing on each data event
+  // console.log(data); // data is a Buffer object
+  const nameVar = data.toString().trim().toUpperCase();
+  process.stdout.write(`Hi ${nameVar}!`);
 });
