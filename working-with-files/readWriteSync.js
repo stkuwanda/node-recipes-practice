7a882d28c3cf096c0e259e5e1c5a8d826da803b4
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-19 17:40:09 +0200

    used alternatively path.resolve method to flexibly create a path for the txt file to be read

--- working-with-files/readWriteSync.js
+++ working-with-files/readWriteSync.js
@@ -1,11 +1,12 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-const filePath = path.join(process.cwd(), 'hello.txt');
+// const filePath = path.join(process.cwd(), 'hello.txt');
+const filePath = path.resolve(process.cwd(), 'hello.txt'); // | path.resolve(process.cwd(),'./', 'hello.txt');
 
 const contents = fs.readFileSync(filePath, 'utf8');
 console.log('File contents:', contents);
 
 const contentToUppercase = contents.toUpperCase();
 fs.writeFileSync(filePath, contentToUppercase); // overwrites the file with updated text
-console.log('File update.');
+console.log('File updated.');
