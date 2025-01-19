da157e00c8904d0004db66cdcc2c0874c342c3e7
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-19 18:36:40 +0200

    pure promises with async/ await syntax

--- working-with-files/readWrite_promisified.js
+++ working-with-files/readWrite_promisified.js
@@ -5,10 +5,23 @@ const filePath = path.resolve(process.cwd(), 'hello.txt');
 
 const readFile = fs.readFile(filePath, 'utf8');
 
-readFile
-	.then(function (content) {
+// pure promises version
+// readFile
+// 	.then(function (content) {
+// 		console.log('File content:', content);
+// 	})
+// 	.catch(function (err) {
+// 		console.log(err);
+// 	});
+
+// pure promises with async/ await syntax
+async function run() {
+	try {
+		const content = await readFile;
 		console.log('File content:', content);
-	})
-	.catch(function (err) {
-		console.log(err);
-	});
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+run();
