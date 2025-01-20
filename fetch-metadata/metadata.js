83f67f8a79ed0bd88818da7596a85d80ae94c790
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-20 07:12:05 +0200

    added error handling inside the printFileMetaData() function

--- fetch-metadata/metadata.js
+++ fetch-metadata/metadata.js
@@ -3,14 +3,17 @@ const fs = require('node:fs');
 
 // prints file metadata
 function printFileMetaData(file) {
-  const fileStats = fs.statSync(file);
-  console.log(fileStats);
+	try {
+		const fileStats = fs.statSync(file);
+		console.log(fileStats);
+	} catch (err) {
+		console.error('Error reading file path:', file);
+	}
 }
 
 const file = process.argv[2]; // read file name as a command-line argument
 printFileMetaData(file);
 
 // launch program in the terminal:
-// node metadata.js file.txt
-
-
+// node metadata.js file.txt // existing file
+// node metadata.js non_existent_file.txt //  test error handling
