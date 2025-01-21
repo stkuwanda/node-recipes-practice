66722d3408a9cdccb346d015a92eae520080d579
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-21 13:27:11 +0200

    convert a buffer to a JSON object representation

--- buffers/buffer.js
+++ buffers/buffer.js
@@ -6,7 +6,7 @@ const buffer = new ArrayBuffer(8); // argument is 8 bytes
 // the data view is created using a typed array to represent the buffer in a specific format
 // the view will accommodate 2 slots each of size 4 bytes (32 bit signed integer slot)
 // allowing storage of 2 numbers inside the view
-const view = new Int32Array(buffer); 
+const view = new Int32Array(buffer);
 view[0] = 1; // stor a number in the first storage slot
 view[1] = 2; // store a number in the final slot
 view[2] = 3; // no error, but this assignment is ignored as this slot doesn't exist
@@ -14,8 +14,8 @@ view[2] = 3; // no error, but this assignment is ignored as this slot doesn't ex
 console.log(view);
 
 // NodeJs Buffers
-// Buffer objects represent a fixed-length sequence of bytes in Node.js. 
-// The Buffer class is a subclass of the Uint8Array JavaScript class and extends it with 
+// Buffer objects represent a fixed-length sequence of bytes in Node.js.
+// The Buffer class is a subclass of the Uint8Array JavaScript class and extends it with
 // methods that cover additional use cases.
 // Node.js API accepts simple Uint8Arrays if buffers are supported
 
@@ -44,9 +44,9 @@ const buf5 = Buffer.alloc(5);
 console.log('buf5 empty:', buf5);
 
 // the second argument is the offset, determines where to start overwriting the buffer.
-// offset says overwriting begins from and including the third slot or 
+// offset says overwriting begins from and including the third slot or
 // number of bytes to skip before starting to write string. Default: 0.
-// the third parameter is the maximum number of bytes to write (written bytes will not exceed buf.length - offset). 
+// the third parameter is the maximum number of bytes to write (written bytes will not exceed buf.length - offset).
 // Default: buf.length - offset
 buf5.write('hello', 2, 2, 'utf8');
 console.log(buf5);
@@ -62,8 +62,8 @@ Returns: <string>
 const buf6 = Buffer.allocUnsafe(26);
 
 for (let i = 0; i < 26; i++) {
-  // 97 is the decimal ASCII value for 'a'.
-  buf6[i] = i + 97;
+	// 97 is the decimal ASCII value for 'a'.
+	buf6[i] = i + 97;
 }
 
 console.log(buf6.toString('utf8')); // Prints: abcdefghijklmnopqrstuvwxyz
@@ -74,3 +74,12 @@ console.log(buf7.toString('hex')); // Prints: 74c3a97374
 console.log(buf7.toString('utf8', 0, 3)); // Prints: té
 console.log(buf7.toString(undefined, 0, 3)); // Prints: té
 
+// convert a buffer to a JSON object representation
+
+// simple method
+const buf8 = Buffer.from([0x1, 0x2, 0x3, 0x4, 0x5]);
+const jsonString = JSON.stringify(buf8);
+console.log(JSON.parse(jsonString));
+
+// using buf.toJSON() method
+console.log(buf8.toJSON());
