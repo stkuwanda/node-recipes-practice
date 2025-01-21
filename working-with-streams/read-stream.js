0d528b72e1e421aefc325a787e88e9edd894b60c
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-21 18:55:54 +0200

    create a readable stream

---
+++ working-with-streams/read-stream.js
@@ -0,0 +1,14 @@
+// create a readable stream to read from a file
+const fs = require('node:fs');
+
+const rs = fs.createReadStream('./file.txt');
+
+// data event fires each time a new chunk of data is read
+rs.on('data', function (data) {
+	console.log('Read chunk:', data);
+});
+
+// end event fires when there's no data to read from the stream
+rs.on('end', function () {
+  console.log('Read stream ended.');
+});
