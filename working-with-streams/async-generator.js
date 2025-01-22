716e3cdca4fea21e2263f20c6af1140e709474fa
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-22 10:07:33 +0200

    constructing Readable type streams with async iterators

---
+++ working-with-streams/async-generator.js
@@ -0,0 +1,20 @@
+// constructing readable streams with iterators
+const { Readable } = require('node:stream');
+
+async function* generate() {
+	yield 'Node.js';
+	yield 'is';
+	yield 'a';
+	yield 'JavaScript';
+	yield 'Runtime';
+}
+
+const generator = generate();
+
+// creating a readable stream using the Readable.from() API
+const rs = Readable.from(generator);
+
+rs.on('data', function (chunk) {
+  // note, with this Readable type (contrast ReadStream type), the chunk is already a string NOT a buffer
+  console.log(chunk); 
+});
