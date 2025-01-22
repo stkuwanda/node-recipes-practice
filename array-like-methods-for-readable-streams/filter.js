139f675db7e118afc10283bc2f1b3ab772610380
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-22 20:05:34 +0200

    use the readable.filter() method

--- array-like-methods-for-readable-streams/filter.js
+++ array-like-methods-for-readable-streams/filter.js
@@ -1,1 +1,17 @@
+// use the readable.filter() method
 
+const { Readable } = require('node:stream');
+
+async function* generate() {
+	yield 'Java\n';
+	yield 'Python\n';
+	yield 'Golang\n';
+}
+
+const generator = generate();
+
+const rd = Readable.from(generator).filter(function (chunk) {
+	return chunk.length > 4;
+});
+
+rd.pipe(process.stdout);
