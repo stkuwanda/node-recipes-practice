3c5276761220ff9eb2177f07256b216591de3870
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-22 14:53:57 +0200

    added notes on how pipes work and how to keep the destination writables open for continued writing

--- piping-streams/pipe-stream.js
+++ piping-streams/pipe-stream.js
@@ -1,6 +1,11 @@
 // piping readable streams
+// readableStream.pipe() method attaches a data event handler to the source stream,
+// which writes the incoming data to the destination stream.
+// the destination stream must be at least writable (duplex and transform streams are allowed; they're writable too).
+
 const fs = require('node:fs');
 
 const rs = fs.createReadStream('./file.txt');
 
 rs.pipe(process.stdout);
+
