dc61e3858842052ff51734cfa38458fdf8cc086f
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-22 15:42:23 +0200

    creating transform streams using the ES6 Classes

---
+++ transform-streams/transform-streams-es6.js
@@ -0,0 +1,16 @@
+// creating transform streams using the ES6 Classes
+
+const fs = require('node:fs');
+const { Transform } = require('node:stream');
+
+const rs = fs.createReadStream('./file.txt');
+const newFile = fs.createWriteStream('./newFile.txt');
+
+class Uppercase extends Transform {
+	_transform(chunk, encoding, callback) {
+		this.push(chunk.toString().toUpperCase());
+		callback();
+	}
+}
+
+rs.pipe(new Uppercase()).pipe(newFile);
