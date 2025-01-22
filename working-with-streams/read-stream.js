59c85d5d58a220b3d13bb56eae034e9551241470
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-22 07:07:25 +0200

    called toString() method on the data chunk to convert Buffer into String

--- working-with-streams/read-stream.js
+++ working-with-streams/read-stream.js
@@ -5,7 +5,8 @@ const rs = fs.createReadStream('./file.txt');
 
 // data event fires each time a new chunk of data is read
 rs.on('data', function (data) {
-	console.log('Read chunk:', data);
+	// console.log('Read chunk:', data);
+  console.log('Read chunk:', data.toString());
 });
 
 // end event fires when there's no data to read from the stream
